33238f6bf6c198ebaf62c5cf49282652a0682c41
Jordi Ricard Onrubia Palacios <jordirop@gmail.com>
2021-06-13 18:52:56 +0200

    changed colours

--- src/app/home/sponsors/sponsors.component.ts
+++ src/app/home/sponsors/sponsors.component.ts
@@ -8,14 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class SponsorsComponent implements OnInit {
 
   public sponsors: any[] = [
-    {
+/*    {
       web: 'http://www.eps.udl.cat/',
       img: 'assets/eps.png',
-      level: 0
-    },
-    {
-      web: 'http://www.udl.es/ca/',
-      img: 'assets/logo_udl.jpg',
       level: 0
     },
     {
@@ -24,80 +19,15 @@ export class SponsorsComponent implements OnInit {
       level: 1
     },
     {
-      web: 'https://www.invelon.com/',
-      img: 'assets/invelon.png',
-      level: 1
-    },
-    {
-      web: 'https://eurecat.org/es/',
-      img: 'assets/eurecat.png',
-      level: 1
-    },
-    {
-      web: 'http://www.actiumdigital.es/es',
-      img: 'assets/actium.png',
-      level: 1
-    },
-    {
-      web: 'https://origen.studio/',
-      img: 'assets/origenstudio.png',
-      level: 1
-    },
-    {
       web: 'https://www.dominospizza.es',
       img: 'assets/dominos_pizza.png',
-      level: 2
-    },
-    {
-      web: 'https://www.intech3d.es/',
-      img: 'assets/intech3d.png',
-      level: 2
-    },
-    {
-      web: 'https://enginyeriainformatica.cat/',
-      img: 'assets/coinf.png',
-      level: 2
-    },
-    {
-      web: 'https://alumni.udl.cat/alumni/',
-      img: 'assets/alumni.jpg',
-      level: 2
-    },
-    {
-      web: 'http://www.cosantex.com/',
-      img: 'assets/cosantex.png',
       level: 2
     },
     {
       web: 'https://www.jetbrains.com/',
       img: 'assets/jetbrains.svg',
       level: 3
-    },
-    {
-      web: 'http://www.consell-eps.udl.cat/',
-      img: 'assets/consell.jpg',
-      level: 3
-    },
-    {
-      web: 'https://linktr.ee/labicicleta.ads',
-      img: 'assets/bicicleta.png',
-      level: 3
-	},
-	{
-	  web: 'http://www.delsamscafes.com/',
-	  img: 'assets/delsams.png',
-	  level: 3
-	},
-	{
-	  web: 'http://arfonfruits.com/',
-	  img: 'assets/arfon.png',
-	  level: 3
-	},
-	{
-	  web: 'https://es-es.facebook.com/pages/category/Event-Planner/Planeta-Gastro-Eventos-216982095712423/',
-	  img: 'assets/planetagastro.jpeg',
-	  level: 3
-	}
+    },*/
   ];
 
   constructor() { }
